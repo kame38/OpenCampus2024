083e832ec45a6a0f33eb778999388b35506a2869
Kohki-Amemiya <amemiya.kohki.q2@dc.tohoku.ac.jp>
2024-05-20 21:22:58 +0900

    formatted

--- param.py
+++ param.py
@@ -1,5 +1,6 @@
-# ================= take_photo.py ==============================
+# constat parameters
 
+# ================= take_photo.py ==============================
 MIN_LEN = 20  # 検出する直線の最小長さ
 MAX_GAP = 200  # 直線として認識する最大の間隔
 GRAY_THR = 20  # 濃度変化の閾値
@@ -19,14 +20,12 @@ NUM_EPOCHS = 100                        # 学習回数
 LEARNING_RATE = 0.01                    # 学習率
 
 # ================= realtime_classification.py ==============================
-
 CKPT_NET = 'trained_net.ckpt'               # 学習済みパラメータファイル
 OBJ_NAMES = ['alpha', 'cosmic']             # 各クラスの表示名
 # MIN_LEN = 20
 # GRAY_THR = 20
 CONTOUR_COUNT_MAX = 3       # バッチサイズ(一度に検出する物体の数)の上限
 SHOW_COLOR = (255, 191, 0)  # 枠の色(B,G,R)
-
 NUM_CLASSES = 2  # クラス数
 PIXEL_LEN = 112  # Resize後のサイズ(1辺)
 CHANNELS = 1     # 色のチャンネル数(BGR:3, グレースケール:1)
